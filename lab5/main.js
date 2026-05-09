9d19042549a084eee5d3bf0d3a91548b0250864e
Timber Groeneveld <timbergroeneveld@hotmail.com>
2026-05-10 01:15:03 +0200

    remote_wfs

--- lab5/main.js
+++ lab5/main.js
@@ -94,11 +94,11 @@ registerGeoLocate(map)
 // Points of interest (WFS)
  function registerWFSReadAndWriteLayer(mapInstance, toc) {
      // Settings - These need to agree with the definition of the WFS layer in Geoserver
-     var namespace_prefix = "geo1007";
-     var namespace_uri = "http://all.kinds.of.data";
-     var server_url = "http://localhost:8080"
-     var layer_name = "pois"
-     var geom_column_name = "geom"
+	var namespace_prefix = "geo1007";
+	var namespace_uri = "http://all.kinds.of.data";
+	var server_url = "https://varioscale.bk.tudelft.nl"
+	var layer_name = "pois"
+	var geom_column_name = "geom"
      // End Settings
 
      // reading the layer from the WFS
